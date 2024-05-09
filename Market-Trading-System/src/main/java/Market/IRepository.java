becae2ce129fab9bf097709bc0e7c78d2ddfa24a
Israel Vaitzman <israelw@post.bgu.ac.il>
2024-05-09 23:17:53 +0300

    interface implementation

---
+++ Market-Trading-System/src/main/java/Market/IRepository.java
@@ -0,0 +1,11 @@
+package Market;
+
+import java.util.List;
+
+public interface IRepository<T> {
+    T findById(long id);
+    List<T> findAll();
+    void save(T entity);
+    void update(T entity);
+    void delete(T entity);
+}
