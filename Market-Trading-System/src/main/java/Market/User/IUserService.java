449cf411614e5f2b1cbcbc793ea784e3b6b56f3b
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-10 12:12:25 +0300

    add comments about authentication

--- Market-Trading-System/src/main/java/Market/User/IUserService.java
+++ Market-Trading-System/src/main/java/Market/User/IUserService.java
@@ -5,8 +5,8 @@ public interface IUserService{
     public void GuestExit(int GuestID);
     public void register(String userName, String password);
     public void login(String userName, String password);
-    public void logout();
-    public String viewShoppingCart();
-    public void modifyShoppingCart();
-    public void checkoutShoppingCart();
+    public void logout(String userName);
+    public String viewShoppingCart(String token);
+    public void modifyShoppingCart(String token);
+    public void checkoutShoppingCart(String token);
 }
