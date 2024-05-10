631eb61b27661da036319e4dc1662bb385e79940
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-10 13:09:56 +0300

    creates user controller

--- Market-Trading-System/src/main/java/Market/User/UserController.java
+++ Market-Trading-System/src/main/java/Market/User/UserController.java
@@ -1,4 +1,35 @@
 package Market.User;
 
+import Market.IRepository;
+
 public class UserController implements IUserFacade{
+    IRepository<User> users;
+
+    public UserController() {
+    }
+
+    public void createGuestSession(){
+
+    }
+    public void terminateGuestSession(){
+
+    }
+    public void register(String userName, String password){
+
+    }
+    public boolean login(String userName, String password){
+
+    }
+    public void logout(String userName){
+
+    }
+    public String viewShoppingCart(String token){
+
+    }
+    public void modifyShoppingCart(String token){
+
+    }
+    public void checkoutShoppingCart(String token){
+
+    }
 }
