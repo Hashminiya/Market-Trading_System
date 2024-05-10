ed78fdf39041e6c6b732652ed776440cd78953d3
Amit Hassan <amithass@post.bgu.ac.il>
2024-05-10 23:20:47 +0300

    creating functions in IStoreFacade for each use case related to store package

--- Market-Trading-System/src/main/java/DomainLayer/Market/Store/IStoreFacade.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/Store/IStoreFacade.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 public interface IStoreFacade {
     public void createStore(long founderId, String storeName, String storeDescription);
+    public void viewInventory(long storeId);
     public void addItemToStore(long storeId, String itemName, double itemPrice, int stockAmount, List<String> categoryChain);
     public void updateItemName(long storeId, long itemId, String newName);
     public void updateItemPrice(long storeId, long  itemId, double newPrice);
     public void decreaseItemAmount(long storeId, long itemId, int count);
+    public void deleteItem(long storeId, long itemId);
     public void changeStorePolicy(long storeId);
     public void changeDiscountType(long storeId, String newType);
     public void removeStore(long storeId);
-
+    public void viewPurchaseHistory(long storeId);
+    public void getAllProductsInfoByStore(long storeId);
+    public void searchStoreByKeyWord(String keyWord);
+    public void searchItemByKeyWord(String keyWord);
+    public void searchStoreByCategory(String category);
+    public void searchItemByCategory(String category);
+    public void searchStoreByName(String name);
+    public void searchItemByName(String name);
+    public void assignStoreOwner(long userId);
+    public void assignStoreManager(long userId);
 }
