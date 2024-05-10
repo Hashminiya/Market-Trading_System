1666fc6061a4d71f7f87df4fe7eb591f95cf1429
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-10 12:37:13 +0300

    creates UserFacade and defines the basic methods

---
+++ Market-Trading-System/src/main/java/Market/User/UserFacade.java
@@ -0,0 +1,12 @@
+package Market.User;
+
+public interface UserFacade {
+    public void createGuestSession();
+    public void terminateGuestSession();
+    public void register(String userName, String password);
+    public boolean login(String userName, String password);
+    public void logout(String userName);
+    public String viewShoppingCart(String token);
+    public void modifyShoppingCart(String token);
+    public void checkoutShoppingCart(String token);
+}
