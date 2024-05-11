ec145172a3ae696ecab9942189e2e973b312f5b2
idonag <idonag@post.bgu.ac.il>
2024-05-11 20:24:43 +0300

    basic implementation

--- Market-Trading-System/src/main/java/ServiceLayer/Store/StoreManagementService.java
+++ Market-Trading-System/src/main/java/ServiceLayer/Store/StoreManagementService.java
@@ -1,43 +1,132 @@
 package ServiceLayer.Store;
 
+import DomainLayer.Market.Store.IStoreFacade;
+
+import java.util.List;
+
 public class StoreManagementService implements IStoreManagementService{
+    private final IStoreFacade storeFacade;
+    public StoreManagementService(IStoreFacade storeFacade){
+        this.storeFacade = storeFacade;
+    }
+
     @Override
-    public String createStore() {
+    public String createStore(long founderId, String storeName, String storeDescription) {
+        try {
+            storeFacade.createStore(founderId,storeName,storeDescription);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String viewInventory() {
+    public String addItemToStore(long storeId, String itemName, double itemPrice, int stockAmount, List<String> categoryChain) {
+        try {
+            storeFacade.addItemToStore(storeId,itemName,itemPrice,stockAmount,categoryChain);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String modifyItem(Long storeId, Long itemId) {
+    public String updateItemName(long storeId, long itemId, String newName) {
+        try {
+            storeFacade.updateItemName(storeId, itemId, newName);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String deleteItem(Long storeId, Long itemId) {
+    public String updateItemPrice(long storeId, long itemId, double newPrice) {
+        try {
+            storeFacade.updateItemPrice(storeId, itemId, newPrice);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String changePolicy(Long storeId) {
+    public String decreaseItemAmount(long storeId, long itemId, int count) {
+        try {
+            storeFacade.decreaseItemAmount(storeId, itemId, count);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String assignOwner(Long storeId, Long newOwnerId) {
+    public String deleteItem(long storeId, long itemId) {
+        try {
+            storeFacade.deleteItem(storeId, itemId);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String changeManagerPermissions(Long storeId, Long managerId) {
+    public String changeStorePolicy(long storeId) {
+        try {
+            storeFacade.changeStorePolicy(storeId);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+        return null;    }
+
+    @Override
+    public String changeDiscountType(long storeId, String newType) {
+        try {
+            storeFacade.changeDiscountType(storeId, newType);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public String removeStore(Long storeId) {
+    public String assignOwner(long storeId, Long newOwnerId) {
+        try {
+            storeFacade.assignStoreOwner(storeId, newOwnerId);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public String changeManagerPermissions(long storeId, long managerId) {
+        try {
+            storeFacade.(storeId, newOwnerId);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public String removeStore(long storeId) {
+        try {
+            storeFacade.removeStore(storeId);
+        }
+        catch (Exception e){
+            throw new RuntimeException(e.getMessage());
+        }
         return null;
     }
 
@@ -49,5 +138,15 @@ public class StoreManagementService implements IStoreManagementService{
     @Override
     public String viewPurchasesHistory(Long storeId) {
         return null;
+    }
+
+    @Override
+    public void assignStoreOwner(long userId) {
+
+    }
+
+    @Override
+    public void assignStoreManager(long userId) {
+
     }
 }
