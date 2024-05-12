6f3a0c9965547dae02d18082793c02ba8fa7eee4
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-12 23:28:35 +0300

    create ShoppingBasket class

---
+++ Market-Trading-System/src/main/java/DomainLayer/Market/ShoppingBasket.java
@@ -0,0 +1,39 @@
+package DomainLayer.Market;
+
+import DomainLayer.Market.Store.Item;
+
+import java.util.HashMap;
+
+public class ShoppingBasket {
+
+    private HashMap<Integer,Integer> items;
+    private int storeId;
+
+    public ShoppingBasket(){
+        items = new HashMap<>();
+    }
+
+    public void addItem(int itemId, int quantity){
+        items.put(itemId,quantity);
+    }
+
+    public void removeItem(int itemId){
+        items.remove(itemId);
+    }
+
+    public void purchaseShoppingBasket(){
+        //TODO: implement
+    }
+
+    public void updateItemQuantity(int itemId, int quantity){
+        if(quantity == 0)
+            removeItem(itemId);
+        //TODO: check stock?
+        items.put(itemId,quantity);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //TODO: overwrite to return the shopping basket items
+    }
+}
