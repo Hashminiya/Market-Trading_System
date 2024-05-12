984586506b32dfb17099172a5904279e99583e6e
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-13 00:01:33 +0300

    updates register function

--- Market-Trading-System/src/main/java/DomainLayer/Market/User/UserController.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/User/UserController.java
@@ -2,18 +2,23 @@ package DomainLayer.Market.User;
 
 import Market.IRepository;
 
+import java.util.HashMap;
+
 public class UserController implements IUserFacade{
-    private IRepository<User> users;
+    //private IRepository<User> users;
+    HashMap<String,User> users;
 
     public UserController() {
-        users = new UsersRepository();
+        users = new HashMap<>();
     }
 
     public void createGuestSession(){
         int id = generateId();
         State g = new Guest();
         User u = new User(id,g);
-        users.save(id,u,null,null);
+        //TODO: save guest
+
+        // users.save(id,u,null,null);
     }
     public void terminateGuestSession(String token){
         int id = token.extractId();
@@ -21,12 +26,15 @@ public class UserController implements IUserFacade{
         users.delete(u);
     }
 
-    public void register(String token, String userName, String password){
+    public void register(String userName, String password) throws Exception {
         //TODO: check if the username already exist?
-        int id = token.extractId();
-        State r = new Registered();
-        User u = users.findById(id);
-        u.changeState(r);
+        if(users.containsKey(userName)){
+            throw new Exception("username already exist");
+        }
+        //TODO: encrypt password
+        State registered = new Registered();
+        User user = users.get(userName);
+        user.changeState(registered);
     }
     public boolean login(String token, String userName, String password){
         int id = token.extractId();
