dc1c6f56852c82de0344a12a8a06baaad346c1c9
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-13 10:24:43 +0300

    updates functions to use Response object

--- Market-Trading-System/src/main/java/ServiceLayer/User/UserService.java
+++ Market-Trading-System/src/main/java/ServiceLayer/User/UserService.java
@@ -8,19 +8,32 @@ public class UserService implements IUserService {
     IUserFacade facade;
 
     public Response GuestEntry(){
-        facade.CreateGuestSession();
+        try{
+            facade.CreateGuestSession();
+            return Response.ok().build();
+        }
+        catch (Exception e){
+            return Response.serverError().build();
+        }
     }
+
     public Response GuestExit(int GuestID){
-        facade.terminateGuest(GuestID);
+        try{
+            facade.terminateGuest(GuestID);
+            return Response.ok().build();
+        }
+        catch (Exception e){
+            return Response.serverError().build();
+        }
     }
 
     public Response register(String userName, String password){
         try{
             facade.register(userName,password);
-            return Response.ok();
+            return Response.ok().build();
         }
         catch (Exception e){
-            return Response.error(e);
+            return Response.serverError().build();
         }
     }
 
@@ -28,25 +41,26 @@ public class UserService implements IUserService {
         try{
             facade.login(userName,password);
             String token = TokenService.generateToken(userName);
-            return Response.ok(token);
+            return Response.ok(token).build();
         }
         catch (Exception e){
-            return Response.error(e);
+            return Response.serverError().build();
         }
     }
 
     public Response logout(String token){
         try{
             if(TokenService.validateToken(token)){
+                String userName = TokenService.extractUserName(token);
                 facade.logout(userName);
-                return Response.ok();
+                return Response.ok().build();
             }
             else{
-                return Response.unauthorized();
+                return Response.status(401).build();
             }
         }
         catch (Exception e){
-            return Response.error(e);
+            return Response.serverError().build();
         }
     }
 
@@ -54,14 +68,14 @@ public class UserService implements IUserService {
         try{
             if(TokenService.validateToken(token)){
                 String res = facade.viewShoppingCart(token);
-                return Response.ok(res);
+                return Response.ok(res).build();
             }
             else{
-                return Response.unauthorized();
+                return Response.status(401).build();
             }
         }
         catch (Exception e){
-            return Response.error(e.getMessage());
+            return Response.serverError().build();
         }
     }
 
@@ -69,28 +83,28 @@ public class UserService implements IUserService {
         try{
             if(TokenService.validateToken(token)){
                 facade.modifyShoppingCart(token);
-                return Response.ok();
+                return Response.ok().build();
             }
             else{
-                return Response.unauthorized();
+                return Response.status(401).build();
             }
         }
         catch (Exception e){
-            return Response.error(e.getMessage());
+            return Response.serverError().build();
         }
     }
     public Response checkoutShoppingCart(String token) {
         try{
             if(TokenService.validateToken(token)){
                 facade.checkoutShoppingCart(token);
-                return Response.ok();
+                return Response.ok().build();
             }
             else{
-                return Response.unauthorized();
+                return Response.status(401).build();
             }
         }
         catch (Exception e){
-            return Response.error(e.getMessage());
+            return Response.serverError().build();
         }
     }
 }
