8f8cf16eb128d2aad1a391ca173cac5876d1381c
Israel Vaitzman <israelw@post.bgu.ac.il>
2024-05-14 12:38:20 +0300

    create a inital interface and controller for the purchase package

---
+++ Market-Trading-System/src/main/java/DomainLayer/Market/Purchase/IPurchaseFacade.java
@@ -0,0 +1,12 @@
+package DomainLayer.Market.Purchase;
+
+import java.util.Date;
+import java.util.List;
+
+public interface IPurchaseFacade {
+    boolean initServices();
+    boolean isValidServices();
+    boolean checkout(int userID, int creditCard, Date expiryDate , int cvv , List<ItemDTO> purchaseItemsList);
+    List<Purchase> getPurchaseByStore(int storeId);
+    List<Purchase> getPurchaseByUser(int UserId);
+}
