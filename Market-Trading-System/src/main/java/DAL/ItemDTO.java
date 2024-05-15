33cb330698f979764ce0763e23cede5c44f2222e
Yagilbr <yagilbr@bgu.post.ac.il>
2024-05-15 21:00:38 +0300

    update ItemDTO fields and addition of getters

--- Market-Trading-System/src/main/java/DAL/ItemDTO.java
+++ Market-Trading-System/src/main/java/DAL/ItemDTO.java
@@ -4,4 +4,26 @@ public class ItemDTO {
     private int storeId;
     private int totalPrice;
     private int quantity;
+    private int itemName;
+    private int itemId;
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public int getItemName() {
+        return itemName;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
 }
