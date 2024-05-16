e8a5ec29b66712dfe1f8e4e030168323f118ac73
Yagilbr <yagilbr@bgu.post.ac.il>
2024-05-16 10:02:12 +0300

    implementing checkout at Purchase

--- Market-Trading-System/src/main/java/DomainLayer/Market/Purchase/Purchase.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/Purchase/Purchase.java
@@ -1,7 +1,11 @@
 package DomainLayer.Market.Purchase;
 
+import DAL.ItemDTO;
 import DomainLayer.Market.Purchase.OutServices.PaymentServiceImpl;
 import DomainLayer.Market.Purchase.OutServices.SupplyServiceImpl;
+
+import java.util.Date;
+import java.util.List;
 
 public class Purchase implements IPurchase{
         private PaymentServiceProxy paymentServiceProxy;
@@ -14,8 +18,26 @@ public class Purchase implements IPurchase{
             receipt = new Receipt();
         }
 
+
     @Override
-    public void checkout() {
-        //TODO: implement this
+    public boolean checkout(List<ItemDTO> itemList, String creditCard, Date expiryDate, String CVV) {
+        Boolean isValidCard = paymentServiceProxy.validateCreditCard(creditCard,expiryDate,CVV);
+        Boolean canBeSupplied = supplyServiceProxy.validateCartSupply(itemList);
+        if(isValidCard & canBeSupplied){
+            double amount = calculateAmount(itemList);
+            paymentServiceProxy.chargeCreditCard(creditCard,expiryDate,CVV,amount);
+            supplyServiceProxy.supplyCart(itemList);
+            return true;
+        }
+        return false;
+    }
+
+    public double calculateAmount(List<ItemDTO> itemList) {
+        double sum=0;
+        for (ItemDTO item:itemList) {
+            sum+=item.getTotalPrice();
+        }
+        return sum;
     }
 }
+
