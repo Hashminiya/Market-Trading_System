08299c80cb80cf52fb1deea9ef72f43447a79d23
Yagilbr <yagilbr@bgu.post.ac.il>
2024-05-18 12:23:44 +0300

    add exceptions to Purchase Controller

--- Market-Trading-System/src/main/java/DomainLayer/Market/Purchase/PurchaseController.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/Purchase/PurchaseController.java
@@ -15,9 +15,9 @@ import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
-public class PurchaseController implements IPurchaseFacade{
-    private HashMap<Long,List<ItemDTO>> storeIDtoItems;
-    private HashMap<String,List<ItemDTO>> userIDtoItems;
+public class PurchaseController implements IPurchaseFacade {
+    private HashMap<Long, List<ItemDTO>> storeIDtoItems;
+    private HashMap<String, List<ItemDTO>> userIDtoItems;
     private BlockingQueue<ItemDTO> purchasedItems;
 
 
@@ -30,20 +30,20 @@ public class PurchaseController implements IPurchaseFacade{
     private boolean isPaymentServiceConnected;
     private boolean isSupplyServiceConnected;
 
-    public PurchaseController(){
-        isPaymentServiceConnected =false;
-        isSupplyServiceConnected=false;
+    public PurchaseController() {
+        isPaymentServiceConnected = false;
+        isSupplyServiceConnected = false;
 
         storeIDtoItems = new HashMap<>();
-        userIDtoItems=new HashMap<>();
-        purchasedItems = new PriorityBlockingQueue<ItemDTO>() ; //protected queue
+        userIDtoItems = new HashMap<>();
+        purchasedItems = new PriorityBlockingQueue<ItemDTO>(); //protected queue
         initServices();
     }
 
     @Override
     public void initServices() {
-        paymentServiceImpl=new PaymentServiceImpl();
-        supplyServiceImpl=new SupplyServiceImpl();
+        paymentServiceImpl = new PaymentServiceImpl();
+        supplyServiceImpl = new SupplyServiceImpl();
 
         paymentServiceProxy = new PaymentServiceProxy(paymentServiceImpl);
         supplyServiceProxy = new SupplyServiceProxy(supplyServiceImpl);
@@ -56,18 +56,18 @@ public class PurchaseController implements IPurchaseFacade{
 
     @Override
     public boolean checkout(String userID, String creditCard, Date expiryDate, String cvv, List<ItemDTO> purchaseItemsList) {
-        Purchase purchase = new Purchase(paymentServiceProxy,supplyServiceProxy);
-        boolean success = purchase.checkout(purchaseItemsList,creditCard,expiryDate,cvv);
-        if(!success)
+        Purchase purchase = new Purchase(paymentServiceProxy, supplyServiceProxy);
+        boolean success = purchase.checkout(purchaseItemsList, creditCard, expiryDate, cvv);
+        if (!success)
             return false;
-        for (ItemDTO item:purchaseItemsList) { // save all the purchased items
-            if(!userIDtoItems.containsKey(userID))
-                userIDtoItems.put(userID,new ArrayList<>());
+        for (ItemDTO item : purchaseItemsList) { // save all the purchased items
+            if (!userIDtoItems.containsKey(userID))
+                userIDtoItems.put(userID, new ArrayList<>());
             userIDtoItems.get(userID).add(item);//save item by userID
 
-            if(!storeIDtoItems.containsKey(item.getStoreId()))
+            if (!storeIDtoItems.containsKey(item.getStoreId()))
                 storeIDtoItems.put(item.getStoreId(), new ArrayList<>());
-            storeIDtoItems.get(item.getStoreId()).add(item);
+            storeIDtoItems.get(item.getStoreId()).add(item);//save item by storeID
             purchasedItems.add(item);
         }
         return true;
@@ -75,26 +75,29 @@ public class PurchaseController implements IPurchaseFacade{
 
     @Override
     public List<ItemDTO> getPurchasesByStore(long storeId) {
-        if(!storeIDtoItems.containsKey(storeId))
-            return null;
+        if (!storeIDtoItems.containsKey(storeId))
+            throw new RuntimeException(String.format("In store: %d no product has been bought", storeId));
         return storeIDtoItems.get(storeId);
     }
 
     @Override
     public List<ItemDTO> getPurchasesByUser(String userId) {
-        if(!userIDtoItems.containsKey(userId))
-            return null;
+        if (!userIDtoItems.containsKey(userId))
+            throw new RuntimeException(String.format("user: %s did not buy anything", userId));
         return userIDtoItems.get(userId);
     }
 
     @Override
-    public List<ItemDTO> getPurchasedItems() {
+    public synchronized List<ItemDTO> getPurchasedItems() {
         List<ItemDTO> purchasedItems = new ArrayList<>();
-        synchronized (this) {
-            while (this.purchasedItems.size() > 0)
-                purchasedItems.add(this.purchasedItems.remove());
-        }
+        if (this.purchasedItems.size() == 0)
+            throw new RuntimeException("There are no purchased items waiting to reduce from inventory");
+        while (this.purchasedItems.size() > 0)
+            purchasedItems.add(this.purchasedItems.remove());
         return purchasedItems;
     }
 }
+
+
+
 
