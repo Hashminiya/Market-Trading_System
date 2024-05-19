57502259cf8ac7dad1fdcc781ae48f84ccda0544
Noam Shurki <shurkin@post.bgu.ac.il>
2024-05-19 18:02:31 +0300

    updates

--- Market-Trading-System/src/main/java/DomainLayer/Market/User/Istate.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/User/Istate.java
@@ -1,6 +1,11 @@
 package DomainLayer.Market.User;
 
+import DomainLayer.Market.Util.StorePermission;
+
+import java.util.List;
+
 public interface Istate {
-    public boolean checkPermission(Permission permission);
-    public List<Permission> getPermission();
+    public boolean checkPermission(String permission, Long storeId);
+    public boolean checkPermission(String permission);
+    public List<StorePermission> getPermission();
 }
