bde67e276f33ef661d06fcac47ca92e54a83dc5f
noamgilad <noamgilad98@gmail.com>
2024-05-21 08:35:33 +0300

    Refactor User class for more organized structure

--- Market-Trading-System/src/main/java/DomainLayer/Market/User/User.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/User/User.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-
 public class User implements DataItem<String> {
     private String userName;
     private String password;
@@ -16,13 +15,47 @@ public class User implements DataItem<String> {
     private ShoppingCart shoppingCart;
     private Map<Long, Set<StorePermission>> storePermissions;
 
-    public User(String userName,String password, int userAge, Istate state, boolean loggedIn, ShoppingCart shoppingCart) {
+    public User(String userName, String password, int userAge, Istate state, boolean loggedIn, ShoppingCart shoppingCart) {
         this.userName = userName;
         this.password = password;
         this.userAge = userAge;
         this.state = state;
         this.loggedIn = loggedIn;
         this.shoppingCart = shoppingCart;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public Istate getUserState() {
+        return state;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
+
+    @Override
+    public String getId() {
+        return userName;
+    }
+
+    @Override
+    public String getName() {
+        return userName;
     }
 
     public void setUserName(String userName) {
@@ -45,78 +78,47 @@ public class User implements DataItem<String> {
         this.shoppingCart = shoppingCart;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public int getUserAge() {
-        return userAge;
-    }
-
-    public Istate getUserStates() { return state; }
-
-    public boolean isLoggedIn() { return loggedIn; }
-
-    public ShoppingCart getShoppingCart() { return shoppingCart; }
-
-
-    public boolean login(String userName, String password){
-        if(this.userName.equals(userName) && this.password.equals(password)){
+    public boolean login(String userName, String password) {
+        if (this.userName.equals(userName) && this.password.equals(password)) {
             loggedIn = true;
             return true;
         }
         return false;
     }
 
-    public boolean logout(String userName){
-        if(this.userName.equals(userName)){
+    public boolean logout(String userName) {
+        if (this.userName.equals(userName)) {
             loggedIn = false;
             return true;
         }
         return false;
     }
 
-
-
-    public boolean addToShoppingCart(int storeNum, int productNum, int quantity){
-        if(loggedIn){
+    public boolean addToShoppingCart(int storeNum, int productNum, int quantity) {
+        if (loggedIn) {
             return shoppingCart.addToShoppingCart(storeNum, productNum, quantity);
         }
         return false;
     }
 
-    public boolean deleteShoppingBasket(int storeNum, int productNum){
-        if(loggedIn){
+    public boolean deleteShoppingBasket(int storeNum, int productNum) {
+        if (loggedIn) {
             return shoppingCart.deleteShoppingBasket(storeNum, productNum);
         }
         return false;
     }
 
-    public boolean modifyShoppingCart(int storeNum, int productNum, int newQuantity){
-        if(loggedIn){
+    public boolean modifyShoppingCart(int storeNum, int productNum, int newQuantity) {
+        if (loggedIn) {
             return shoppingCart.modifyShoppingCart(storeNum, productNum, newQuantity);
         }
         return false;
     }
 
-    public boolean checkOutShoppingCart(){
-        if(loggedIn){
+    public boolean checkOutShoppingCart() {
+        if (loggedIn) {
             return shoppingCart.checkOutShoppingCart();
         }
         return false;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getId() {
-        return userName;
-    }
-
-    @Override
-    public String getName() {
-        return userName;
     }
 }
