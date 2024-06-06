a974f559ec186c27e94149c46d82d08e895bbc1f
Amit Hassan <amithass@post.bgu.ac.il>
2024-06-06 23:23:49 +0300

    fixing constructors

--- Market-Trading-System/src/main/java/DomainLayer/Market/Store/Discount/HiddenDiscount.java
+++ Market-Trading-System/src/main/java/DomainLayer/Market/Store/Discount/HiddenDiscount.java
@@ -2,14 +2,15 @@ package DomainLayer.Market.Store.Discount;
 
 import java.security.cert.CertificateExpiredException;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public class HiddenDiscount extends Discount {
 
     private String code;
 
-    public HiddenDiscount(Long id, double percent, Date expirationDate, long storeId, String code){
-        super(id, percent, expirationDate, storeId);
+    public HiddenDiscount(Long id, double percent, Date expirationDate, long storeId, List<Long> items, List<String> categories, String code){
+        super(id, percent, expirationDate, storeId, items, categories);
         this.code = code;
     }
 
