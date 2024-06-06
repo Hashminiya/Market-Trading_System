313528b0b40beaf4031c6cdc4525324ed6791d00
Noam Shurki <shurkin@post.bgu.ac.il>
2024-06-06 23:22:05 +0300

    changes mapping in storeControllerApi classes

--- Market-Trading-System/src/main/java/API/controller/StoreManagementControllerApi.java
+++ Market-Trading-System/src/main/java/API/controller/StoreManagementControllerApi.java
@@ -6,9 +6,7 @@ import ServiceLayer.Store.StoreBuyerService;
 import ServiceLayer.Store.StoreManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.core.Response;
 import java.util.HashMap;
@@ -24,37 +22,37 @@ public class StoreManagementControllerApi {
         this.storeManagementService = storeManagementService;
     }
 
-    @GetMapping("storeManagement/createStore")
+    @PostMapping("storeManagement/createStore")
     public ResponseEntity<String> createStore(@RequestParam String founderToken, @RequestParam String storeName, @RequestParam String storeDescription,@RequestParam IRepository<Long, Discount> repository) {
         return storeManagementService.createStore(founderToken, storeName, storeDescription, repository);
     }
 
-    @GetMapping("storeManagement/addItemToStore")
+    @PutMapping("storeManagement/addItemToStore")
     public ResponseEntity<String> addItemToStore(@RequestParam String token,@RequestParam long storeId,@RequestParam String itemName,@RequestParam String description,@RequestParam double itemPrice,@RequestParam int stockAmount,@RequestParam List<String> categories) {
         return storeManagementService.addItemToStore(token, storeId, itemName, description, itemPrice, stockAmount, categories);
     }
 
-    @GetMapping("storeManagement/updateItem")
+    @PatchMapping("storeManagement/updateItem")
     public ResponseEntity<String> updateItem(@RequestParam String token,@RequestParam long storeId,@RequestParam long itemId,@RequestParam String newName,@RequestParam double newPrice,@RequestParam int newAmount) {
         return storeManagementService.updateItem(token, storeId, itemId, newName, newPrice, newAmount);
     }
 
-    @GetMapping("storeManagement/deleteItem")
+    @DeleteMapping("storeManagement/deleteItem")
     public ResponseEntity<String> deleteItem(@RequestParam String token,@RequestParam long storeId,@RequestParam long itemId) {
         return storeManagementService.deleteItem(token, storeId, itemId);
     }
 
-    @GetMapping("storeManagement/changeStorePolicy")
+    @PatchMapping("storeManagement/changeStorePolicy")
     public ResponseEntity<String> changeStorePolicy(@RequestParam String token,@RequestParam long storeId) {
         return storeManagementService.changeStorePolicy(token, storeId);
     }
 
-    @GetMapping("storeManagement/changeDiscountType")
+    @PatchMapping("storeManagement/changeDiscountType")
     public ResponseEntity<String> changeDiscountType(@RequestParam String token,@RequestParam long storeId,@RequestParam String newType) {
         return storeManagementService.changeDiscountType(token, storeId, newType);
     }
 
-    @GetMapping("storeManagement/removeStore")
+    @DeleteMapping("storeManagement/removeStore")
     public ResponseEntity<String> removeStore(@RequestParam String token,@RequestParam long storeId) {
         return storeManagementService.removeStore(token, storeId);
     }
@@ -74,12 +72,12 @@ public class StoreManagementControllerApi {
         return storeManagementService.viewPurchasesHistory(token, storeId);
     }
 
-    @GetMapping("storeManagement/assignStoreOwner")
+    @PostMapping("storeManagement/assignStoreOwner")
     public ResponseEntity<String> assignStoreOwner(@RequestParam String token,@RequestParam long storeId,@RequestParam String newOwnerId) {
         return storeManagementService.assignStoreOwner(token, storeId, newOwnerId);
     }
 
-    @GetMapping("storeManagement/assignStoreManager")
+    @PostMapping("storeManagement/assignStoreManager")
     public ResponseEntity<String> assignStoreManager(@RequestParam String token,@RequestParam long storeId,@RequestParam String newManagerI,@RequestParam List<String> permissions) {
         return storeManagementService.assignStoreManager(token, storeId, newManagerI, permissions);
     }
