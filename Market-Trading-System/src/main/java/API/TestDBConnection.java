133900222454639e8edcc2736c2f3a75a3a81a3a
Noam Shurki <shurkin@post.bgu.ac.il>
2024-06-22 13:37:55 +0300

    connect the server to a google sql DB

---
+++ Market-Trading-System/src/main/java/API/TestDBConnection.java
@@ -0,0 +1,20 @@
+package API;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestDBConnection {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://34.172.134.83:3306/hashminia";
+        String username = "dbManager";
+        String password = "dbManager";
+        try {
+            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+            System.out.println("Connected to the database!");
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
