0670fd7cf1d294a27187d5065c3f5e84d50c5363
Noam Shurki <shurkin@post.bgu.ac.il>
2024-06-27 21:06:22 +0300

    adds items and basket deletion on shutdown

--- Market-Trading-System/src/main/java/API/ShutdownHandler.java
+++ Market-Trading-System/src/main/java/API/ShutdownHandler.java
@@ -1,8 +1,5 @@
 package API;
-import DomainLayer.Repositories.DbStoreRepository;
-import DomainLayer.Repositories.DbUserRepository;
-import DomainLayer.Repositories.StoreRepository;
-import DomainLayer.Repositories.UserRepository;
+import DomainLayer.Repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.EventListener;
@@ -23,5 +20,13 @@ public class ShutdownHandler {
         System.out.println("Deleting all stores...");
         stores.deleteAll();
         System.out.println("All stores deleted.\n");
+        ItemRepository items = SpringContext.getBean(DbItemRepository.class);
+        System.out.println("Deleting all items...");
+        items.deleteAll();
+        System.out.println("All items deleted.\n");
+        BasketRepository baskets = SpringContext.getBean(DbBasketRepository.class);
+        System.out.println("Deleting all baskets...");
+        baskets.deleteAll();
+        System.out.println("All baskets deleted.\n");
     }
 }
