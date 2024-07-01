4bff8a7bb7b446d5cd98cd4ae854a8ffa5460cca
idonag <idonag@post.bgu.ac.il>
2024-07-01 18:56:31 +0300

    supoort view of all policies in a store

---
+++ Market-Trading-System/src/main/java/DAL/PolicyDTO.java
@@ -0,0 +1,41 @@
+package DAL;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PolicyDTO {
+    private long id;
+    private String name;
+    private String type;
+
+    @JsonCreator
+    public PolicyDTO(@JsonProperty("id") long id, @JsonProperty("name") String name, @JsonProperty("type") String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
